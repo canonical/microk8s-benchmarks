41dc5e6435ef029547adff8c5d4cde991baa8765
Ferran Llamas <llamas.arroniz@gmail.com>
2022-05-26 10:20:12 +0200

    Add more unit tests

---
+++ scripts/scale-test/tests/unit/test_juju.py
@@ -0,0 +1,28 @@
+from unittest.mock import patch
+
+import pytest
+
+from benchmarks.juju import run
+
+JUJU_MODULE = "benchmarks.juju"
+
+
+@patch(f"{JUJU_MODULE}._juju")
+def test_run(_juju):
+    command = "some command"
+
+    # wrong input values
+    with pytest.raises(ValueError):
+        run(command)
+
+    with pytest.raises(ValueError):
+        run(command, unit="foo", all=True)
+
+    # a specific unit
+    run(command, unit="foo")
+    _juju.assert_called_once_with("run", "-u", "foo", "--", command)
+    _juju.reset_mock()
+
+    # all units
+    run(command, all=True)
+    _juju.assert_called_once_with("run", "-a", "--", command)
