d444a3782c454080d8e6be20f8d027ff4fdad5d8
Ferran Llamas <llamas.arroniz@gmail.com>
2022-05-31 12:30:05 +0200

    Improve install_microk8s

--- scripts/scale-test/setup_cluster.py
+++ scripts/scale-test/setup_cluster.py
@@ -6,7 +6,7 @@ import os
 from argparse import ArgumentParser, Namespace
 from typing import List, Optional
 
-from benchmarks import juju
+from benchmarks.clients import juju
 from benchmarks.constants import DEFAULT_ADD_NODE_TOKEN, DEFAULT_ADD_NODE_TOKEN_TTL
 from benchmarks.models import Cluster, DockerCredentials, Unit
 from benchmarks.utils import timeit
@@ -22,25 +22,56 @@ logging.basicConfig(format=LOG_FORMAT, level=logging.INFO, datefmt=LOG_DATEFMT)
 
 @timeit
 def install_microk8s(
-    model: str,
     units: List[Unit],
     channel: str = DEFAULT_CHANNEL,
     http_proxy: Optional[str] = None,
     creds: Optional[DockerCredentials] = None,
 ):
-    if http_proxy:
-        configure_http_proxy(http_proxy)
-
-    reboot_and_wait(model)
     install_snap(channel)
     update_etc_hosts(units)
 
-    if creds:
-        configure_containerd(creds)
-        restart_microk8s_on_nodes()
+    if creds or http_proxy:
+        configure_containerd(creds, http_proxy)
+        restart_containerd()
+
+    wait_microk8s_ready()
 
 
-def configure_containerd(creds: DockerCredentials):
+def restart_containerd():
+    cmd = "sudo snap restart microk8s.daemon-containerd"
+    juju.run(cmd, app=APP_NAME).check_returncode()
+
+
+def configure_containerd(
+    creds: Optional[DockerCredentials] = None, http_proxy: Optional[str] = None
+):
+    """
+    Configure http proxy if specified. Configure docker credentials if
+    specified and http_proxy not set - rate limits are not a problem
+    behind a http proxy.
+    """
+    if http_proxy:
+        _configure_containerd_proxy(http_proxy)
+    elif creds:
+        _configure_containerd_credentials(creds)
+
+
+def _configure_containerd_proxy(http_proxy: str):
+    logging.info("Configuring containerd proxy settings")
+    containerd_env = "/var/snap/microk8s/current/args/containerd-env"
+    commands = ";".join(
+        [
+            f"echo HTTPS_PROXY={http_proxy} >> {containerd_env}",
+            f"echo HTTP_PROXY={http_proxy} >> {containerd_env}",
+            "juju_instance_id=$(grep \"juju\" /etc/hosts | head -n 1 | awk '{print $NF}')",
+            'noproxy="10.0.0.0/8,127.0.0.0/8,192.168.0.0/16,${juju_instance_id}"',
+            f"echo NO_PROXY=${{noproxy}} >> {containerd_env}",
+        ]
+    )
+    juju.run(commands, app=APP_NAME).check_returncode()
+
+
+def _configure_containerd_credentials(creds: DockerCredentials):
     logging.info("Configuring containerd docker credentials")
     containerd_file = "/var/snap/microk8s/current/args/containerd-template.toml"
     lines = [
@@ -72,34 +103,20 @@ def update_etc_hosts(units: List[Unit]):
 @timeit
 def install_snap(channel: str):
     logging.info("Installing microk8s on all units")
-    status_timeout = 60 * 20  # 20 min
     all_commands = [
         f"snap install microk8s --classic --channel={channel}",
         "sudo usermod -a -G microk8s ubuntu",
         "sudo chown -f -R ubuntu ~/.kube",
         "sudo newgrp microk8s",
-        f"microk8s status --wait-ready --timeout={status_timeout}",
     ]
     command = ";".join(all_commands)
     juju.run(command, app=APP_NAME).check_returncode()
 
 
 @timeit
-def configure_http_proxy(http_proxy: str):
-    logging.info("Configuring proxy settings on units")
-    commands = ";".join(
-        [
-            f"echo HTTPS_PROXY={http_proxy} >> /etc/environment",
-            f"echo HTTP_PROXY={http_proxy} >> /etc/environment",
-            f"echo https_proxy={http_proxy} >> /etc/environment",
-            f"echo http_proxy={http_proxy} >> /etc/environment",
-            "juju_instance_id=$(grep \"juju\" /etc/hosts | head -n 1 | awk '{print $NF}')",
-            'noproxy="10.0.0.0/8,127.0.0.0/8,192.168.0.0/16,${juju_instance_id}"',
-            "echo no_proxy=${noproxy} >> /etc/environment",
-            "echo NO_PROXY=${noproxy} >> /etc/environment",
-        ]
-    )
-    juju.run(commands, app=APP_NAME).check_returncode()
+def wait_microk8s_ready(timeout_min: int = 10):
+    cmd = f"microk8s status --wait-ready --timeout {timeout_min * 60}"
+    juju.run(cmd, app=APP_NAME).check_returncode()
 
 
 def reboot_and_wait(model: str):
@@ -290,7 +307,6 @@ def main():
     try:
         units = deploy_units(args.model, args.nodes)
         install_microk8s(
-            args.model,
             units,
             channel=args.channel,
             http_proxy=args.http_proxy,
